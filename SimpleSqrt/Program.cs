6f9ba424cf0cf513eff11ddf5ff91e925dc26c62
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-01-09 12:06:53 +0200

    Updated

---
+++ SimpleSqrt/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            double a = Math.Sqrt(12345);
+            Console.WriteLine(a);
+        }
+    }
+}
