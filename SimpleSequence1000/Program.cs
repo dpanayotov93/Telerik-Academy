3a1e018063ee48c994ef7d5b47dcf2ffc5c6c51d
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-01-09 13:15:09 +0200

    Updated

---
+++ SimpleSequence1000/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimpleSequence
+{
+    class SimpleSequence
+    {
+        static void Main()
+        {
+            for (double i = 2; i <= 1000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(-i);
+                }
+            }
+        }
+    }
+}
