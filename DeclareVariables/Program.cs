f98812da58243e08ee59ce5a5cae35f30045c07f
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-01-13 16:27:36 +0200

    Lecture 1 - Intro to Programming

---
+++ DeclareVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DeclareVariables
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte vByte = 97;
+            sbyte vSbyte = -115;
+            short vShort = -10000;
+            ushort vUshort = 52130;
+            int VInt = 4825932;
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", vByte, vSbyte, vShort, vUshort, VInt);
+        }
+    }
+}
