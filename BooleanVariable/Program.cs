a4e7bf7531ac8ffc81bbdaf616eadd0ece7b41bd
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-01-13 23:58:57 +0200

    Lecture 2 - Primite Data Types and Variables

---
+++ BooleanVariable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BooleanVariable
+{
+    class BooleanVariable
+    {
+        static void Main()
+        {
+            bool gender = true;
+            Console.WriteLine("I am male: {0}", gender);
+        }
+    }
+}
---
+++ StringsАndObjects/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringsАndObjects
+{
+    class StringsАndObjects
+    {
+        static void Main()
+        {
+            string hello = "Hello";
+            string world = "world";
+            object bothWords = hello + " " + world;
+            string typecasting = (string)bothWords;
+
+            Console.WriteLine(typecasting);
+        }
+    }
+}
