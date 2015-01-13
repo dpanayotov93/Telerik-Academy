e808b6cb0c54f66b8cd9d83e7de4b95553fccaaa
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-01-14 00:43:22 +0200

    Lecture 2 - Primite Data Types and Variables

---
+++ ExchangeVariableValues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExchangeVariableValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            int c;
+
+            Console.WriteLine("a={0}, b={1}", a, b);
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("a={0}, b={1}", a , b);
+        }
+    }
+}
