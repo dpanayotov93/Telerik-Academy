a38f898c5cd1aa7877836aab35c4b41988a36467
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-01-14 00:37:40 +0200

    Lecture 2 - Primite Data Types and Variables

---
+++ IsoscelesTriangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace IsoscelesTriangle
+{
+    class IsoscelesTriangle
+    {
+        static void Main()
+        {
+            char copyright = '\u00A9';
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine(" " + " " + " " + copyright);
+            Console.WriteLine(" " + " " + copyright + " " + copyright);
+            Console.WriteLine(" " + copyright + " " + " " + " " + copyright);
+            Console.WriteLine(copyright + " " + copyright + " " + copyright + " " + copyright);
+        }
+    }
+}
