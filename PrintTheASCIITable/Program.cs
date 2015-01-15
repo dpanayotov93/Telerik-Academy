db6d550997d73de409f2635be3ca610355f6de29
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-01-15 15:05:59 +0200

    Lecture 2 - Primite Data Types and Variables

---
+++ PrintTheASCIITable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace PrintTheASCIITable
+{
+    class PrintTheASCIITable
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.ASCII;
+            for (int numberInTable = 0; numberInTable <= 255; numberInTable++)
+            {
+                char character = (char)numberInTable;
+                Console.WriteLine("{0} = {1}", numberInTable, character);
+            }
+        }
+    }
+}
