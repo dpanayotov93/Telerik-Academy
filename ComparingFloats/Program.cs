16fe583a315fbd3c9dcefc1ad823cf97408f3a03
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-01-15 15:25:59 +0200

    Lecture 2 - Primite Data Types and Variables

---
+++ ComparingFloats/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ComparingFloats
+{
+    class ComparingFloats
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please, enter the first number that you want to compare: ");
+            double first = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please, enter the second number that you want to compare: ");
+            double second = double.Parse(Console.ReadLine());
+            double eps = 0.000001;
+            bool comparison;
+
+            if ((Math.Abs(first - second)) < eps)
+            {
+                comparison = true;
+            }
+            else
+            {
+                comparison = false;
+            }
+          
+            Console.Clear();
+            Console.WriteLine("The numbers are the same: {0}", comparison);
+        }
+    }
+}
