2a71789984781ae526e44d2b88e8d2a759f79e5c
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-01-15 15:53:55 +0200

    Lecture 2 - Primite Data Types and Variables

---
+++ NullValuesArithmetic/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Problem 12. Null Values Arithmetic
+
+    Create a program that assigns null values to an integer and to a double variable.
+    Try to print these variables at the console.
+    Try to add some number or the null literal to these variables and print the result. 
+ */
+
+using System;
+
+namespace NullValuesArithmetic
+{
+    class NullValuesArithmetic
+    {
+        static void Main()
+        {
+            int? randomInt = null;
+            double? randomDouble = null;
+
+            Console.WriteLine("The Null Integer is: {0}" + System.Environment.NewLine + "The Null Double is: {1}", randomInt, randomDouble);
+            Console.WriteLine("Int + 2 = {0}", (randomInt + 2));
+            Console.WriteLine("Double + 2 = {0}", (randomDouble + 2));
+
+        }
+    }
+}
